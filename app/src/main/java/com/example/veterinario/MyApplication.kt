6056886d202287690db7c62651a543558b39fb37
Alonso Rivadeneira <al.rivadeneira@duocuc.cl>
2025-10-26 18:13:41 -0300

    Configuración de la clase de aplicación y ajustes del manifiesto

--- app/src/main/java/com/example/veterinario/MyApplication.kt
+++ app/src/main/java/com/example/veterinario/MyApplication.kt
@@ -1,2 +1,10 @@
 package com.example.veterinario
 
+import android.app.Application
+import com.example.veterinario.data.AppDatabase
+import com.example.veterinario.data.AnimalRepository
+
+class MyApplication : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { AnimalRepository(database.animalDao()) }
+}
