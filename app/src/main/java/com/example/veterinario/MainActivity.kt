1b91e51d1fded1571545a2b67048e63a4ec1a16c
Alonso Rivadeneira <al.rivadeneira@duocuc.cl>
2025-10-26 18:23:12 -0300

    Inicialización del ViewModel y Navegación

--- app/src/main/java/com/example/veterinario/MainActivity.kt
+++ app/src/main/java/com/example/veterinario/MainActivity.kt
@@ -3,45 +3,38 @@ package com.example.veterinario
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.veterinario.uiApp.AppNavigation
 import com.example.veterinario.ui.theme.VeterinarioTheme
+import com.example.veterinario.viewmodel.AnimalViewModel
+import com.example.veterinario.viewmodel.AnimalViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
+    // 1. MODIFICA LA CREACIÓN DEL VIEWMODEL
+    // Ahora le pasamos 'application' a la Factory
+    private val viewModel: AnimalViewModel by viewModels {
+        AnimalViewModelFactory(
+            (application as MyApplication).repository,
+            application // <-- Pasa la 'application' aquí
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             VeterinarioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation(viewModel = viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VeterinarioTheme {
-        Greeting("Android")
     }
 }
