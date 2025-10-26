aaddbc864974c2f024e1c60755046f0fc6bdfbc8
Alonso Rivadeneira <al.rivadeneira@duocuc.cl>
2025-10-26 17:04:16 -0300

    Implementación del repositorio y la base de datos

--- app/src/main/java/com/example/veterinario/data/AnimalRepository.kt
+++ app/src/main/java/com/example/veterinario/data/AnimalRepository.kt
@@ -1,2 +1,20 @@
 package com.example.veterinario.data
 
+import kotlinx.coroutines.flow.Flow
+
+class AnimalRepository(private val animalDao: AnimalDao) {
+
+    val todosLosAnimales: Flow<List<Animal>> = animalDao.getTodos()
+
+    fun getAnimal(id: Int): Flow<Animal> {
+        return animalDao.getPorId(id)
+    }
+
+    suspend fun insertar(animal: Animal) {
+        animalDao.insertar(animal)
+    }
+
+    suspend fun actualizar(animal: Animal) {
+        animalDao.actualizar(animal)
+    }
+}
