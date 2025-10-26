f69104a0a794abc4abccd43e79a78ee4aa51beb7
Constanza Rojas <cons.rojasb@duocuc.cl>
2025-10-26 16:56:04 -0300

    agregue codigo en animal.kt

--- app/src/main/java/com/example/veterinario/data/Animal.kt
+++ app/src/main/java/com/example/veterinario/data/Animal.kt
@@ -1,2 +1,17 @@
+
 package com.example.veterinario.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "animales")
+data class Animal(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nombre: String,
+    val tipo: String, // "Perro", "Gato", etc.
+    val edad: Int,
+    val descripcion: String,
+    val fotoUri: String, // Path de la imagen
+    val adoptado: Boolean = false
+)
